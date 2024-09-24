feed9b079dce2dae9e2f285a61984214f5f062c4
starvieree <ainurreza90788@gmail.com>
2024-09-23 19:33:22 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Floating Action Button',
+      title: 'Cupertino Button dan Loading Bar',
       home: LoadingCupertino(),
     );
   }
